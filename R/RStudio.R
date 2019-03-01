789e4ad5541246386f589a79174b1ceedc692802
jiemaoxue <jiemx2008@163.com>
2019-03-01 17:31:45 +0800

    test

--- R/RStudio.R
+++ R/RStudio.R
@@ -193,7 +193,7 @@ compileReconnectCode <- function(connection) {
                                     "getMetaData")
   ##url <- rJava::.jcall(databaseMetaData, "Ljava/lang/String;", "getURL")
   ##user <- rJava::.jcall(databaseMetaData, "Ljava/lang/String;", "getUserName")
-  code <- sprintf("con <- connect(connectionDetails)\n")
+  code <- sprintf("con <- connect(connectionDetails)")
   return(code)
 }
 
